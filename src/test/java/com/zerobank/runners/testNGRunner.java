0666ae92f417d9cd13292d0e82e0de3f878ba789
arpat9329 <68973655+arpat9329@users.noreply.github.com>
2020-11-05 19:12:42 -0500

    testNG parallel

--- src/test/java/com/zerobank/runners/testNGRunner.java
+++ src/test/java/com/zerobank/runners/testNGRunner.java
@@ -8,9 +8,13 @@ import org.testng.annotations.DataProvider;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin =  "rerun:target/rerun.txt",
+        plugin =  {"json:target/cucumberTestNG.json",
+                "html:target/cucumber-reportTestNG.html",
+                "rerun:target/rerun.txt"
+        },
         features = "src/test/resources/features",
-        glue = "com/zerobank/stepDefinitions"
+        glue = "com/zerobank/stepDefinitions",
+        publish = true
 )
 
 public class testNGRunner extends AbstractTestNGCucumberTests {
